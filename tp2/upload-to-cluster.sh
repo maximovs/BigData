38a83e7b74381efb1b2ba3380a235bc354f188ca
Alvaro Crespo <alvaro.andres.crespo@gmail.com>
2013-11-17 19:27:05 -0300

    Added script to upload stuff to cluster. Now we have mvn installed in cluster so we shouldnt need it, but anyway...

--- tp2/upload-to-cluster.sh
+++ tp2/upload-to-cluster.sh
@@ -1,3 +1,28 @@
-#tar -zcvf tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh --exclude='target'
-tar -zcvf tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh
+#!/bin/bash
+
+#Quite mode
+TAR_OPTS="-zcf"
+#Verbose mode
+#TAR_OPTS="-zcvf"
+
+if [ $# = 0 ]; then
+	tar $TAR_OPTS tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh
+	echo "No arguments. Taring everything and uploading to cluster. This may take a while..."
+elif [ $# = 1 ] && [ $1 = 'exlude-jars' ]; then
+	tar $TAR_OPTS tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh --exclude='target'
+	echo "Exluding jars. Taring everything except jars and uploading to cluster. This shouldn't take long."
+elif [ $# = 1 ] && [ $1 != 'exlude-jars' ]; then
+	tar $TAR_OPTS tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh --exclude=$1
+	echo "Excluding 1 project - $1, but including jars. Taring everything except jars and uploading to cluster. This may take a while..."
+elif [ $# = 2 ] && [ $1 != 'exlude-jars' ]; then
+	tar $TAR_OPTS tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh --exclude=$1 --exclude=$2
+	echo "Excluding 2 projects - $1 and $2, but including jars. Taring everything except jars and uploading to cluster. This may take a while..."
+elif [ $# = 2 ] && [ $1 = 'exlude-jars' ]; then
+	tar $TAR_OPTS tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh --exclude='target' --exclude=$2
+	echo "Exluding jars and 1 project - $2. Taring everything except jars and uploading to cluster. This shouldn't take long."
+elif [ $# = 3 ] && [ $1 = 'exlude-jars' ]; then
+	tar $TAR_OPTS tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh --exclude='target' --exclude=$2 --exclude=$3
+	echo "Exluding jars and 2 projects - $2 and $3. Taring everything except jars and uploading to cluster. This shouldn't take long."
+fi
+
 scp -i id_dsa tp2.tar.gz hadoop@hadoop-2013-datanode-4:./grupoBarri/
