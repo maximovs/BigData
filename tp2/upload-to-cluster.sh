4223bf8a01b2270582d3cdb17a9fb9ebeabb2199
Alvaro Crespo <alvaro.andres.crespo@gmail.com>
2013-11-17 19:34:25 -0300

    Fixed upload script :)

--- tp2/upload-to-cluster.sh
+++ tp2/upload-to-cluster.sh
@@ -8,19 +8,19 @@ TAR_OPTS="-zcf"
 if [ $# = 0 ]; then
 	tar $TAR_OPTS tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh
 	echo "No arguments. Taring everything and uploading to cluster. This may take a while..."
-elif [ $# = 1 ] && [ $1 = 'exlude-jars' ]; then
+elif [ $# = 1 ] && [ $1 = 'exclude-jars' ]; then
 	tar $TAR_OPTS tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh --exclude='target'
 	echo "Exluding jars. Taring everything except jars and uploading to cluster. This shouldn't take long."
-elif [ $# = 1 ] && [ $1 != 'exlude-jars' ]; then
+elif [ $# = 1 ] && [ $1 != 'exclude-jars' ]; then
 	tar $TAR_OPTS tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh --exclude=$1
 	echo "Excluding 1 project - $1, but including jars. Taring everything except jars and uploading to cluster. This may take a while..."
-elif [ $# = 2 ] && [ $1 != 'exlude-jars' ]; then
+elif [ $# = 2 ] && [ $1 != 'exclude-jars' ]; then
 	tar $TAR_OPTS tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh --exclude=$1 --exclude=$2
 	echo "Excluding 2 projects - $1 and $2, but including jars. Taring everything except jars and uploading to cluster. This may take a while..."
-elif [ $# = 2 ] && [ $1 = 'exlude-jars' ]; then
+elif [ $# = 2 ] && [ $1 = 'exclude-jars' ]; then
 	tar $TAR_OPTS tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh --exclude='target' --exclude=$2
 	echo "Exluding jars and 1 project - $2. Taring everything except jars and uploading to cluster. This shouldn't take long."
-elif [ $# = 3 ] && [ $1 = 'exlude-jars' ]; then
+elif [ $# = 3 ] && [ $1 = 'exclude-jars' ]; then
 	tar $TAR_OPTS tp2.tar.gz flume/ storm/ tweetsStream/ README.md make.sql upload-to-cluster.sh --exclude='target' --exclude=$2 --exclude=$3
 	echo "Exluding jars and 2 projects - $2 and $3. Taring everything except jars and uploading to cluster. This shouldn't take long."
 fi
